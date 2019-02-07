1295029f9c80958506dae399d5eee5e1ee75a9b3
Pragya Rai <pragyar@thoughtworks.com>
2019-02-07 14:31:50 +0530

    Removed request module and added http-proxy to forward requests

--- proxy/server.js
+++ proxy/server.js
@@ -1,38 +1,15 @@
 const express = require('express');
-const request = require('request');
 const app = express();
+const httpProxy = require('http-proxy');
 const PORT = 9000;
-let services = [process.env.SERVICE1, process.env.SERVICE2];
+let counter = 1;
+let services = process.env.SERVICES.split(',') || ['http://myapp:8080'];
 
-
-app.use(express.urlencoded({extended: true}));
-
-app.get('/', (req,res) => {
-    request.get(`http://${services[0]}:8080${req.url}`, (err,response, body) => {
-        res.send(body);
-    });
-    services = services.reverse();
+app.use((req, res) => {
+    const proxyServer = httpProxy.createProxyServer({ target: services[counter % services.length] });
+    proxyServer.on('error', () => res.end());
+    counter++;
+    proxyServer.proxyRequest(req, res);
 });
 
-app.get('/getNumber', (req,res) => {
-    request.get(`http://${services[0]}:8080${req.url}`, (err, response, body) => {
-        res.json(body);
-    });
-    services = services.reverse();
-});
-
-app.post('/postNumber', (req, res) => {
-    request.post({
-        headers: {'content-type' : 'application/x-www-form-urlencoded'},
-        url:     `http://${services[0]}:8080${req.url}`,
-        body:    `number=${req.body.number}`
-    }, function(error, response, body){
-        if(error){
-            console.log(error);
-        };
-        res.end();
-      });
-      services = services.reverse();
-});
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));      
+app.listen(PORT, () => console.log(`Proxy is listening on port ${PORT}`));      
