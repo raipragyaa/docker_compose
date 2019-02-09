412fc2834c1830a667bc97d3e2cbae2ec921f546
Pragya Rai <pragyar@thoughtworks.com>
2019-02-09 12:55:09 +0530

    Introduced health end point to check the health of service

--- proxy/server.js
+++ proxy/server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const httpProxy = require('http-proxy');
+const request = require('request');
 const PORT = 9000;
 let counter = 1;
-let services = process.env.SERVICES.split(',') || ['http://myapp:8080'];
+let services = process.env.SERVICES.split(',');
+let healthyServices = services;
+
+let getHealthyservices = function(){
+    healthyServices = [];
+    services.forEach(service => {
+       request.get(`${service}/health`, (err, res) => {
+           if (res && res.statusCode === 200){
+               healthyServices.push(service);
+           };
+       });
+    });
+};
+
+setInterval(getHealthyservices,1000);
 
 app.use((req, res) => {
-    const proxyServer = httpProxy.createProxyServer({ target: services[counter % services.length] });
+    const proxyServer = httpProxy.createProxyServer({ target: healthyServices[counter % healthyServices.length] });
     proxyServer.on('error', () => res.end());
     counter++;
     proxyServer.proxyRequest(req, res);
 });
 
 app.listen(PORT, () => console.log(`Proxy is listening on port ${PORT}`));      
+
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const {Client} = require('pg');
+const { Client } = require('pg');
 const PORT = 8080;
+const SECRET_MESSAGE = process.env.SERVICE_MESSAGE || "I your node app";
 const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/postgres';
 
 const client = new Client(connectionString);
 const app = express();
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${SECRET_MESSAGE}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200);
   res.send(`<form method="post" action="/postNumber">  Number:<input type='number' name='number'/>` +
-      `<input type='submit'/></form>`)
+    `<input type='submit'/></form>`)
 });
 
 app.get('/getNumber', (req, res) => {
- client.query('SELECT * FROM numbers', null, (err, result) => {
-    if(err){
+
+  client.query('SELECT * FROM numbers', null, (err, result) => {
+    if (err) {
       console.log(err.message);
       res.status(500);
       res.end();
@@ -25,14 +32,24 @@ app.get('/getNumber', (req, res) => {
     };
     res.status(200);
     res.json(result.rows);
-   });
+  });
+});
+
+app.get('/slow', (req, res) => {
+  let start = Date.now();
+  let end = start + 5000;
+  while (Date.now() < end) {
+
+  };
+  res.status(200);
+  res.send('<h2>Sorry!! Your response is little late :) </h2>');
 });
 
 app.post('/postNumber', (req, res) => {
   let number = req.body.number;
   let insertQuery = `INSERT INTO numbers VALUES(${number})`;
-  client.query( insertQuery, null, (err, result)=> {
-    if(err){
+  client.query(insertQuery, null, (err, result) => {
+    if (err) {
       console.log(err.message);
       res.status(500);
     }
@@ -43,19 +60,23 @@ app.post('/postNumber', (req, res) => {
   });
 });
 
+app.get('/health', (req, res) => {
+  res.status(200);
+  res.end();
+});
 
 client.connect()
-.then(function(){
-  client.query('CREATE TABLE IF NOT EXISTS numbers(number integer);', (err, result)=> {
-    if(err){
-      console.log(`error occured while creating table ${err.message}`);
-    }
-    else{
-      app.listen(PORT, () => console.log(`listening on port ${PORT}`)); 
-    }
+  .then(function () {
+    client.query('CREATE TABLE IF NOT EXISTS numbers(number integer);', (err, result) => {
+      if (err) {
+        console.log(`error occured while creating table ${err.message}`);
+      }
+      else {
+        app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+      }
+    });
+  })
+  .catch(function (err) {
+    console.log(err.message);
   });
-})
-.catch(function(err){
-  console.log(err.message);
-});
 
